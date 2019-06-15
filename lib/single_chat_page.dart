2f1f6e096b5c8ca1cd27236eb9163884ad449b5f
Etornam Sunu <sunumacbright@gmail.com>
2019-06-15 05:23:46 -0500

    setup project structure

---
+++ lib/single_chat_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SingleChatPage extends StatefulWidget {
+  SingleChatPage({Key key}) : super(key: key);
+
+  _SingleChatPageState createState() => _SingleChatPageState();
+}
+
+class _SingleChatPageState extends State<SingleChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
